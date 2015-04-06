d6eda6929e8075f97547cbc4c4fd9519f9094cfa
andreasDL <andreasdelille@hotmail.Com>
2015-04-06 15:37:06 +0200

    models file + database in localdir

--- api.py
+++ api.py
@@ -1,50 +1,18 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from models import Movie, getMovie, addDummyData
 import simplejson
 
 app = Flask(__name__)
-Engine = create_engine('sqlite:////tmp/media.sqlite3', convert_unicode=True)
-Session = sessionmaker(bind=Engine)
-session = Session()
-Base = declarative_base()
+
+@app.route('/addTestData')
+def addDummyStuffz():
+	addDummyData()
+	return 'done', 200
 
 @app.route('/movie/all')
 def getAllMovies():
-	movies = session.query(Movie).all()
-	json = []
-	for movie in movies:
-		json.append({
-			'id' : movie.id,
-			'name' : movie.name,
-			'thumbPath' : movie.thumbPath,
-			'moviePath' : movie.moviePath
-		})
-
-	return simplejson.dumps(json) , 200
-
-
-class Movie(Base):
-	__tablename__ = 'movies'
-	id = Column(Integer, primary_key = True)
-	name = Column(String(80), unique=True)
-	thumbPath = Column(String(150), unique=True)
-	moviePath = Column(String(150), unique=True)
-
-	def __init__(self, name,thumbPath, moviePath):
-		self.name = name
-		self.thumbPath = thumbPath
-		self.moviePath = moviePath
-
-
-Base.metadata.create_all(Engine)
-
-#add example movie
-#movie = Movie(name='hoi',thumbPath='hllk',moviePath='filmpad')
-#session.add(movie)
-#session.commit()
+	return simplejson.dumps(getMovie()) , 200
 
 #run app
 if __name__ == '__main__':

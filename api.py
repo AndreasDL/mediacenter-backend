c631fd1f5cd7e86994210b4936721b3071598f58
andreasDL <andreasdelille@hotmail.Com>
2015-04-07 18:00:40 +0200

    series complete

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, Response
 from flask.ext.sqlalchemy import SQLAlchemy
-from models import Movie, getMovie, addDummyData
+from models import getMovie, getSerie, getEpisode, getEpisodesBySerie
+from models import addDummyData
 import simplejson
 
 app = Flask(__name__)
@@ -10,9 +11,34 @@ def addDummyStuffz():
 	addDummyData()
 	return 'done', 200
 
-@app.route('/movie/all')
-def getAllMovies():
-	return simplejson.dumps(getMovie()) , 200
+@app.route('/movie/<movieid>')
+def getMovieByID(movieid):
+	movies = getMovie(movieid)
+	if movies == None:
+		return 'no movies found', 404
+	else:
+		return Response(simplejson.dumps(movies), content_type='application/json') , 200
+
+@app.route('/serie/<serieid>')
+def getSerieByID(serieid):
+	series = getSerie(serieid)
+	if series == None:
+		return 'no series found', 404
+	else:
+		return Response(simplejson.dumps(series), content_type='application/json'), 200
+
+@app.route('/episode/<episodeid>')
+def getEpisodeByID(episodeid):
+	episodes = getEpisode(episodeid)
+	if episodes == None:
+		return "no episodes found", 404
+	else:
+		return Response(simplejson.dumps(episodes), content_type='application/json'), 200
+
+@app.route('/episode/bySerieID/<serieid>')
+def getEpisodesBySerieID(serieid):
+	episodes = getEpisodesBySerie(serieid)
+	return Response(simplejson.dumps(episodes), content_type='application/json'), 200
 
 #run app
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -12,26 +12,63 @@ session = Session()
 Base = declarative_base()
 
 def getMovie(id=None):
-	movies = []
-	if id == None:
+	json = None
+
+	if id == None or id.upper() == 'ALL':
 		movies = session.query(Movie).all()
+		json = []
+		for movie in movies:
+			json.append(movie.toDict())
 	else:
-		movies = session.query(Movie).filter_by(id=id).first()
+		movie = session.query(Movie).filter_by(id=id).first()
+		if movie != None:
+			json = movie.toDict()  
 		
+	return json
+
+def getSerie(id=None):
+	json = None
+	if id == None or id.upper() == 'ALL':
+		series = session.query(Serie).all()
+		json = []
+		for serie in series:
+			json.append(serie.toDict())
+	else:
+		serie = session.query(Serie).filter_by(id=id).first()
+		if serie != None:
+			json = serie.toDict()
+	return json
+
+def getEpisode(id=None):
+	json = None
+	if id == None or id.upper() == 'ALL':
+		episodes = session.query(Episode).all()
+		json = []
+		for episode in episodes:
+			json.append(episode.toDict())
+	else:
+		episode = session.query(Episode).filter_by(id=id).first()
+		if episode != None:
+			json = episode.toDict()
+	return json
+
+def getEpisodesBySerie(serieid):
+	episodes = session.query(Episode).filter_by(serie=serieid).all()
 	json = []
-	for movie in movies:
-		json.append({
-			'id' : movie.id,
-			'name' : movie.name,
-			'thumbPath' : movie.thumbPath,
-			'moviePath' : movie.moviePath
-		})
+	for episode in episodes:
+		json.append(episode.toDict())
 	return json
 
 def addDummyData():
 	#add example movie
-	movie = Movie(name='hoi',thumbPath='hllk',moviePath='filmpad')
-	session.add(movie)
+	for i in range(6):
+		movie = Movie(name='movie' + str(i),thumbPath='hllk' + str(i),moviePath='filmpad' + str(i))
+		session.add(movie)
+		serie = Serie(name='top gear' + str(i))
+		session.add(serie)
+		for j in range(5):
+			episode = Episode(name='name', serie=i, season=j,epNumber=0)
+			session.add(episode)
 	session.commit()
 
 # ------------------------------------
@@ -49,10 +86,22 @@ class Movie(Base):
 		self.thumbPath = thumbPath
 		self.moviePath = moviePath
 
+	def toDict(self):
+		return {
+			'id' : self.id, 'name' : self.name,
+			'thumbPath' : self.thumbPath, 'moviePath' : self.moviePath
+		}
+
 class Serie(Base):
 	__tablename__ = 'series'
 	id = Column(Integer, primary_key=True)
 	name = Column(String(80),unique=True,nullable=False)
+
+	def __init__(self,name):
+		self.name = name
+
+	def toDict(self):
+		return { 'id' : self.id, 'name' : self.name}
 
 class Episode(Base):
 	__tablename__ = 'episodes'
@@ -63,6 +112,18 @@ class Episode(Base):
 	
 	serie = Column(Integer, ForeignKey('series.id'))
 	season = Column(Integer, nullable = False)
-	number = Column(Integer, nullable = False)
+	epNumber = Column(Integer, nullable = False)
+
+	def __init__(self,name,serie,season,epNumber):
+		self.name = name
+		self.serie = serie
+		self.season = season
+		self.epNumber = epNumber
+
+	def toDict(self):
+		return {
+			'id' : self.id , 'name' : self.name,
+			'serie' : self.serie, 'epNumber' : self.epNumber
+		}
 	
 Base.metadata.create_all(Engine)
